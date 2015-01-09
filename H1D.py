7442ddbca0d1003ff7aa3327c623af628c482441
Iwan-Zotow <iwanzotow@gmail.com>
2015-01-09 13:54:39 -0500

    Variance collection per bin

--- H1D.py
+++ H1D.py
@@ -2,6 +2,7 @@ class H1D(object):
 
     DATA = 0 # data index
     NOE  = 1 # number of events index
+    VAR  = 2 # bin variance index
 
     # constructor
     def __init__(self, n, lo, hi):
@@ -21,7 +22,7 @@ class H1D(object):
         self._underflow = (0.0, 0)
         self._overflow  = (0.0, 0)
 
-        self._data     = [ (0.0, 0) for k in range(self._n)]
+        self._data     = [ (0.0, 0, 0.0) for k in range(self._n)]
 
     # base functionality - filling
     def fill(self, x, weight = 1.0):
@@ -36,16 +37,16 @@ class H1D(object):
         idx = self.find_bin(x)
 
         if (idx < 0):
-            t = (self._underflow[H1D.DATA] + weight, self._underflow[H1D.NOE] + 1)
+            t = (self._underflow[H1D.DATA] + weight, self._underflow[H1D.NOE] + 1, self._underflow[H1D.VAR] + weight*weight)
             self._underflow = t
             return
 
         if (idx >= self._n):
-            t = (self._overflow[H1D.DATA] + weight, self._overflow[H1D.NOE] + 1)
+            t = (self._overflow[H1D.DATA] + weight, self._overflow[H1D.NOE] + 1, self._overflow[H1D.VAR] + weight*weight)
             self._overflow = t
             return
 
-        t = (self._data[idx][H1D.DATA] + weight, self._data[idx][H1D.NOE] + 1)
+        t = (self._data[idx][H1D.DATA] + weight, self._data[idx][H1D.NOE] + 1, self._data[idx][H1D.VAR] + weight*weight)
         self._data[idx] = t
         return
 
@@ -71,6 +72,9 @@ class H1D(object):
 
     def integral(self):
         return self._integral
+
+    def variance(self):
+        return self._variance
 
     def nof_events(self):
         return self._nof_events
